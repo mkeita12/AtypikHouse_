2e572f1faf2b4d51eec4eafd23d23308375951ce
HAdjoke <mkeita94500@gmail.com>
2022-07-04 10:58:32 +0200

    front

---
+++ frontend/components/layouts.js
@@ -0,0 +1,11 @@
+const layouts = {
+    'primary':'#6689f1',
+    'secondary':'#6f38ce',
+    'bgcolor':'f1ecec',
+    'danger':'red',
+
+    'paddingHorizontal': 25,
+    'paddingVertical':78,
+}
+
+export default layouts ;
