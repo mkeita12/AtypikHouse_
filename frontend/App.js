a7fe66101cb586e487749a20a842cef3953e95b7
maxcorval <vidjinelodji@yahoo.com>
2022-07-04 10:43:56 +0200

    Ajout de la NavBar et HomeScreen du Frontend

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import NavBar from './components/NavBar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on my app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavBar/>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
